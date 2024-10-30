bf08a6d88b6b3f1789f7b625636581f12766aff1
Albino Cordeiro <albino@intuitionlogic.com>
2024-10-30 15:09:47 -0400

    warmup

--- labs/misc/warmup/solution.cpp
+++ labs/misc/warmup/solution.cpp
@@ -1,10 +1,19 @@
 
 #include "solution.h"
 
+#ifndef SOLUTION
 int solution(int *arr, int N) {
+  
   int res = 0;
   for (int i = 0; i < N; i++) {
     res += arr[i];
   }
   return res;
 }
+#endif
+
+#ifdef SOLUTION
+int solution(int *arr, int N) {
+  return (1+N)*N/2;
+}
+#endif
--- tools/make_benchmark_library.sh
+++ tools/make_benchmark_library.sh
@@ -7,7 +7,7 @@ cd benchmark
 # Make a build directory to place the build output.
 cmake -E make_directory "build"
 # Generate build system files with cmake.
-cmake -E chdir "build" cmake -DCMAKE_BUILD_TYPE=Release ../
+cmake -E chdir "build" cmake -DCMAKE_CXX_FLAGS="-Wno-c++17-attribute-extensions -Wno-error=c++-attribute-extensions" -DCMAKE_BUILD_TYPE=Release ../
 # or, starting with CMake 3.13, use a simpler form:
 # cmake -DCMAKE_BUILD_TYPE=Release -S . -B "build"
 # Build the library.
